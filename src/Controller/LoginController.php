a01e8c508a69f39b3a2c87426250ddf08e089654
Soufiane El Alami <soufianeelalami1991@gmail.com>
2025-01-21 11:19:43 +0100

    Page de détail d'un objet scss

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function index(): Response
-    {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
-        ]);
-    }
+    // #[Route('/login', name: 'app_login')]
+    // public function index(): Response
+    // {
+    //     return $this->render('login/index.html.twig', [
+    //         'controller_name' => 'LoginController',
+    //     ]);
+    // }
 }
